994d6a6429d8b59434ae9abbf6d79c59b456815e
nahojjjen <johansw95@gmail.com>
2015-02-25 12:18:48 +0100

    Created color palette accessable by getmethods
    Stored in IMat.java

--- src/imat/IMat.java
+++ src/imat/IMat.java
@@ -5,6 +5,7 @@
  */
 package imat;
 
+import java.awt.Color;
 import java.util.List;
 import se.chalmers.ait.dat215.project.*;
 
@@ -16,6 +17,14 @@ import se.chalmers.ait.dat215.project.*;
  */
 public class IMat {
 
+    private static final Color headerColor = new Color(123,123,240);
+    private static final Color headerShadowColor = new Color(80,80,160);
+    private static final Color highlightButtonColor = new Color(200,200,255);
+    
+    private static final Color backgroundColor = new Color(123,123,123);
+    private static final Color averageColor = new Color(230,230,230);
+    private static final Color foregroundColor = new Color(245,245,245);
+    
     private static boolean loggedIn = false;
     private static MainWindow program;
     /**
@@ -44,5 +53,47 @@ public class IMat {
     }
     public static void setLoggedin(boolean isLoggedin){
         loggedIn = isLoggedin;
+    }
+
+    /**
+     * @return the headerColor
+     */
+    public static Color getHeaderColor() {
+        return headerColor;
+    }
+
+    /**
+     * @return the headerShadowColor
+     */
+    public static Color getHeaderShadowColor() {
+        return headerShadowColor;
+    }
+
+    /**
+     * @return the highlightButtonColor
+     */
+    public static Color getHighlightButtonColor() {
+        return highlightButtonColor;
+    }
+
+    /**
+     * @return the backgroundColor
+     */
+    public static Color getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    /**
+     * @return the averageColor
+     */
+    public static Color getAverageColor() {
+        return averageColor;
+    }
+
+    /**
+     * @return the foregroundColor
+     */
+    public static Color getForegroundColor() {
+        return foregroundColor;
     }
 }
